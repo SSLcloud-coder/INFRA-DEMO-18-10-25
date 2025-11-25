1277113fbd78c44a35c1d0be701d978fe37becea
SSLcloud-coder <sslcloudlearning@gmail.com>
2025-11-25 13:20:10 +0530

    101 updated (#9)

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_virtual_network" "vnet-101" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
   address_space       = ["10.0.0.32/16"]
@@ -16,6 +17,7 @@ resource "azurerm_virtual_network" "vnet=102" {
   location           = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
