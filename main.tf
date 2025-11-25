501376a3f57bf1ab33ca16e00015a6b7b8dc92aa
ssl-cloud <sslcloudlearning@gmail.com>
2025-11-25 13:01:42 +0530

    102 updated

--- main.tf
+++ main.tf
@@ -10,6 +10,12 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+resource "azurerm_virtual_network" "vnet=102" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.32/16"]
+  location           = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
